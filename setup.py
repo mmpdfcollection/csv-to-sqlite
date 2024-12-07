998ee2e09294acc41083570ebfc7a65eadc7972c
mmpdfcollection <145756062+mmpdfcollection@users.noreply.github.com>
2024-12-07 17:07:40 +0630

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ setup(
     url="https://github.com/mmpdfcollection/csv-to-sqlite.git",  # GitHub repository URL
     packages=find_packages(),  # Automatically find all Python packages in your project
     include_package_data=True,  # Include files specified in MANIFEST.in
-    install_requires=[
-        "tkinter",  # Required for GUI
+    install_requires=[  # Remove tkinter if it's included by default
+        # "tkinter",  # Uncomment if tkinter is not installed by default on certain systems
     ],
-    classifiers=[
+    classifiers=[  # Add classifiers to specify package details for PyPI
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",  # Specify minimum Python version
-    entry_points={
+    entry_points={  # If you have CLI commands for your library
         "console_scripts": [
             "csv-to-sqlite-gui=csv_to_sqlite.gui:run_gui",  # CLI command to run GUI
         ],
